917b5aa57458008754a2a23def4011a57782aeba
Merve <mervex332@gmail.com>
2026-05-18 17:57:24 +0200

    Adding : - Title with JLabel - Connected the methode for the button action with Gui-class

--- src/Gui.java
+++ src/Gui.java
@@ -4,8 +4,9 @@ import java.awt.*;
 public class Gui extends JFrame{
 
     JFrame jf = new JFrame();
-    JPanel p = new JPanel();
-    JPanel p1 = new JPanel();
+    JPanel p = new JPanel(); //Panel for Button 1
+    JPanel p1 = new JPanel(); //Panel for TextField 1
+    JPanel p2 = new JPanel(); //Panel for Txt 1
 
     public static int width = 1080, height = 900;
 
@@ -14,6 +15,7 @@ public class Gui extends JFrame{
 
         JButton button = createButton();
         JTextField textField = createTextField();
+        JLabel label1 = showTitle();
 
         /*Fenster View*/
         jf.setTitle("Test Fenster");
@@ -26,24 +28,31 @@ public class Gui extends JFrame{
         p.add(button);
         jf.add(p, BorderLayout.CENTER);
 
-        /* TextField View */
+        /* TextField 1 View */
         p1.add(textField);
         jf.add(p1,BorderLayout.WEST);
+
+        /*Text 1*/
+        p2.add(label1);
+        jf.add(p2,BorderLayout.NORTH);
 
     }
 
     /* Button 1 */
     public JButton createButton(){
 
-        JButton button = new JButton("Test");
+        JButton button = new JButton("Button 1");
 
-        button.setPreferredSize(new Dimension(100,40));
+        button.setPreferredSize(new Dimension(100,20));
 
         button.setFocusable(false);
 
 
         ImageIcon printIcon = new ImageIcon("icons8-stop-24.png");
         button.setIcon(printIcon);
+
+        //Connect to actions
+        button.addActionListener(new actions());
 
         return button;
     }
@@ -55,5 +64,15 @@ public class Gui extends JFrame{
         textField.setPreferredSize(new Dimension(250,20));
 
         return textField;
+    }
+
+
+    public JLabel showTitle(){
+
+        JLabel label1 = new JLabel("Test with Java Swing"); //Add Text
+        label1.setBounds(100, 50, 200, 30); //Set Location
+
+
+        return label1;
     }
 }
--- src/actions.java
+++ src/actions.java
@@ -1,10 +1,11 @@
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class actions {
+public class actions implements ActionListener {
 
     /*Button 1 Action*/
     public void actionPerformed(ActionEvent e) {
-        if(e.getActionCommand().equals("Test")){
+        if(e.getActionCommand().equals("Button 1")){
             System.out.println("Button wurde geklickt");
         }
     }
